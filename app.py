47d071b579303fc66df1a4eae4763a77c94aaa45
张尚斌 <zhangshangbin01@spnic.com.cn>
2021-01-07 18:13:17 +0800

    add：增加部分接口

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from dao.fund import Fund
 from dao.user import User
 from scheduler import MyScheduler
-from wxbot import WxBot
-from telebot import TeleBot
+from bots.wxbot import WxBot
+from bots.telebot import TeleBot
 from db_util import DB
 
-from operator import methodcaller
-
 from flask import Flask, request, render_template, session
+import json
 
 app = Flask(__name__)
 db = DB('sqlite3', './data/my_fund.db')
@@ -16,29 +15,69 @@ db = DB('sqlite3', './data/my_fund.db')
 def home():
     return render_template('home.html')
 
-@app.route('/send_fund', methods=['POST'])
-def send_all():
+@app.route('/publish_fund_image/<int:user_id>', methods=['POST'])
+def push_user_funds(user_id):
+    user = next(db.select_data('select * from users where id = %d' % (user_id)))
+    funds = db.select_data('select * from funds where user_id = %d;' % (user_id))
+    bot_id = user.get('bot_id')
+    bot = None
+    'WX: 0, TELE: 1'
+    bot_type = 0
+    if ':' in bot_id:
+        bot = TeleBot(bot_id, user.get('chat_id'))
+        bot_type = 1
+    else:
+        bot = WxBot(bot_id)
+        bot_type = 0
+    for fund in funds:
+        f = Fund({'id' : fund.get('id')})
+        if bot_type == 0:
+            bot.send_image(f.get_fund_byte())
+        else:
+            bot.send_image(f.get_fund_url())
+    return '成功'
+
+@app.route('/publish_fund_image/name/<name>', methods=['POST'])
+def publish_fund_image_by_name(name):
+    user = next(db.select_data('select * from users where name = "%s"' % (name)))
+    funds = db.select_data('select * from funds where user_id = %d;' % (user.get('id')))
+    bot_id = user.get('bot_id')
+    bot = None
+    'WX: 0, TELE: 1'
+    bot_type = 0
+    if ':' in bot_id:
+        bot = TeleBot(bot_id, user.get('chat_id'))
+        bot_type = 1
+    else:
+        bot = WxBot(bot_id)
+        bot_type = 0
+    for fund in funds:
+        f = Fund({'id' : fund.get('id')})
+        if bot_type == 0:
+            bot.send_image(f.get_fund_byte())
+        else:
+            bot.send_image(f.get_fund_url())
+    return '成功'
+
+@app.route('/publish_fund_image', methods=['POST'])
+def publish_fund_image():
     '''
     向所有的bot发送基金曲线
     通过库中查询后提交
     因为基金图片要请求网络，所以通过基金来遍历
     '''
-    res = db.select_data('select f.id, f.user_id, u.name, u.bot_id from funds f, users u where f.user_id = u.id;')
+    res = db.select_data('select f.id, f.user_id, u.name, u.bot_id, u.chat_id from funds f, users u where f.user_id = u.id;')
     '''
     将基金表数据整合为：{
         基金ID1 : [{用户对象}],
         基金ID2 : [{用户对象}]
     }
+    并构造bot字典：{
+        用户id : Bot对象
+    }
     '''
-    funds = dict()
-    users = dict()
-    for r in res:
-        fid = r.get('id')
-        r['id'] = r.get('user_id')
-        if funds.get(fid) != None:
-            funds[fid].append({'users' : User(r)})
-        else:
-            funds[fid] = [{'users':User(r)}]
+    funds = generate_fund_userlist(res)
+    bots = dict()
     print(funds)
     for k, v in funds.items():
         f = Fund({'id': k})
@@ -46,26 +85,40 @@ def send_all():
         img_url = f.get_fund_url()
         for u in v:
             '''
-            查找users字典
+            查找bots字典
             '''
             send_img = img
-            ud = u['users']
-            
-            bot_id = u['users'].bot_id
-            bot = users.get(ud.id)
+            bot_id = u.bot_id
+            bot = bots.get(u.id)
             if bot == None:
                 if ':' in bot_id:
-                    bot = TeleBot(bot_id)
+                    bot = TeleBot(bot_id, u.chat_id)
                 else:
                     bot = WxBot(bot_id)
-                users[ud.id] = bot
+                bots[u.id] = bot
             if ':' in bot.bot_id:
                 send_img = img_url
             else:
                 send_img = img
             bot.send_image(send_img)
-                
     return '成功'
+
+def generate_fund_userlist(data):
+    '''
+    将基金表数据整合为：{
+        基金ID1 : [{用户对象}],
+        基金ID2 : [{用户对象}]
+    }
+    '''
+    fund_user = {}
+    for r in data:
+        fid = r.get('id')
+        r['id'] = r.get('user_id')
+        if fund_user.get(fid) != None:
+            fund_user[fid].append(User(r))
+        else:
+            fund_user[fid] = [User(r)]
+    return fund_user
 
 def __init__():
     db.execute_sql('./db.sql')
