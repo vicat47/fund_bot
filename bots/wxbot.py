849f90f8f5ed404993b5d77a7d631c9f9c80fdc9
张尚斌 <zhangshangbin01@spnic.com.cn>
2021-03-10 17:25:13 +0800

    update: 添加了微信上传文件的接口

--- bots/wxbot.py
+++ bots/wxbot.py
@@ -1,10 +1,14 @@
 import base64
 import hashlib
 import requests
+import re
+import json
 
 class WxBot:
     '第一个参数为指令，第二个参数为机器人的key'
     BOT_URL = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/%s?key=%s'
+    FILE_UPLOAD_URL = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/upload_media?key=%s&type=%s'
+    FILE_UPLOAD_TYPE = 'file'
     def __init__(self, bot_id):
         self.bot_id = bot_id
 
@@ -34,7 +38,25 @@ class WxBot:
             }
         }
         return bot_url, headers, data
+    
+    def upload_file(self, path:str) -> dict:
+        files = {'media': open(path, 'rb')}
+        url = WxBot.FILE_UPLOAD_URL % (self.bot_id, WxBot.FILE_UPLOAD_TYPE)
+        r = requests.post(url, files=files)
+        res = json.loads(r.text)
+        "{'errcode': 0, 'errmsg': 'ok', 'type': 'file', 'media_id': '3k_Jn78qRCH8pp0ftQM04rPe5jbfU7tIbkBzEE2mgcwU', 'created_at': '1615284891'}"
+        return res
 
+    def send_file(self, media_id:str) -> dict:
+        bot_url = WxBot.BOT_URL % ('send', self.bot_id)
+        data = {
+            "msgtype": "file",
+            "file": {
+                "media_id": media_id
+            }
+        }
+        return requests.post(bot_url, json=data)
+        
     async def async_send_image(self, image, session):
         bot_url, headers, data = self.get_image_data(image)
         async with session.post(bot_url, headers=headers, json=data) as res:
